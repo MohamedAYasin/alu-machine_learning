69e47a48e2c95c9c8f5f204a374e4914b9ed23d2
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2024-06-15 20:46:00 +0200

    correlation

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -34,6 +34,10 @@ def correlation(C):
     if C.shape[0] != d or C.shape[1] != d:
         raise ValueError('C must be a 2D square matrix')
 
-    
+    # Calculate the standard deviations
+    std_devs = np.sqrt(np.diag(C))
+
+    # Calculate the correlation matrix
+    correlation_matrix = C / np.outer(std_devs, std_devs)
 
     return correlation_matrix
