1f05ac3b43eefefb9685feff53f5410d52c5769c
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2024-09-16 18:45:43 +0200

    placeholders

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -13,6 +13,6 @@ def create_placeholders(nx, classes):
         that returns two placeholders,
         x and y, for the neural network
     '''
-    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
+    x = tf.placeholder(tf.float, shape=(None, nx), name='x')
     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
     return x, y
