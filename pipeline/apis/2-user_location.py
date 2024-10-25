8a3679e4dc8bbb3e121a2f5c514cdb76541e6525
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2024-10-25 20:38:25 +0200

    Update 2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,48 @@
 #!/usr/bin/env python3
-""" Prints the location of a GitHub user specified
-as an arg to the script
-"""
+'''
+Prints the location of a user
+'''
+
+
 import sys
 import requests
-import json
+import time
+
+
+def get_user_location(api_url):
+    """
+    Fetch and print the location of a GitHub user.
+
+    :param api_url: The API URL for the user
+    """
+    try:
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            user_data = response.json()
+            location = user_data.get('location')
+            if location:
+                print(location)
+            else:
+                print('Location not available')
+        elif response.status_code == 404:
+            print('Not found')
+        elif response.status_code == 403:
+            reset_time = int(
+                response.headers.get('X-RateLimit-Reset', time.time()))
+            current_time = int(time.time())
+            wait_time = (reset_time - current_time) // 60
+            print('Reset in {} min'.format(wait_time))
+        else:
+            print('Error: {}'.format(response.status_code))
+    except requests.RequestException as e:
+        print('An error occurred: {}'.format(e))
+
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
+    if len(sys.argv) != 2:
+        print('Usage: ./2-user_location.py <api_url>')
+        sys.exit(1)
 
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        print(data['location'])
-    elif response.status_code == 403:
-        print('Reset in ')
-    elif response.status_code == 404:
-        print('Not found')
-    else:
-        print('Error:', response.status_code)
+    api_url = sys.argv[1]
+    get_user_location(api_url)
