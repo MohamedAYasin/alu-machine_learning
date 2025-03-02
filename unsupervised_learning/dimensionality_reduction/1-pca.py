9f37a8a5c3c933c3fe1dee28526622791a2929b5
Mohamed Ahmed Yasin <116743995+MohamedAYasin@users.noreply.github.com>
2025-03-02 19:17:33 +0200

    Update 1-pca.py

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,35 +1,20 @@
 #!/usr/bin/env python3
-"""PCA on a dataset"""
+'''
+    A function def pca(X, ndim):
+    that performs PCA on a dataset
+'''
+
+
 import numpy as np
 
 
 def pca(X, ndim):
-    """Perform PCA on a dataset to reduce it to ndim dimensions."""
-    # cnter the dataset
-    X_centered = X - np.mean(X, axis=0)  # is centers the dataset by
-    # subtracting th mean of each feature.
-
-    # calculate the covarance matrix
-    cov_matrix = np.cov(X_centered, rowvar=False)
-
-    # perform eigendecomposition
-    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
-
-    # sort the eigenvalues and corresponding eigenvectors
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    sorted_eigenvectors = eigenvectors[:, sorted_indices]
-
-    # flip the sign of the eigenvectors to match expected
-    # output (might fix error)
-    max_abs_cols = np.argmax(np.abs(sorted_eigenvectors), axis=0)
-    signs = np.sign(sorted_eigenvectors[max_abs_cols,
-                    range(sorted_eigenvectors.shape[1])])
-    sorted_eigenvectors *= signs
-
-    # select the first ndim eigenvectors
-    W = sorted_eigenvectors[:, :ndim]
-
-    # transform the dataset
-    T = np.dot(X_centered, W)
-
-    return T
+    '''
+    performs PCA on a dataset
+    '''
+    mean = np.mean(X, axis=0, keepdims=True)
+    A = X - mean
+    u, s, v = np.linalg.svd(A)
+    W = v.T[:, :ndim]
+    T = np.matmul(A, W)
+    return (T)
